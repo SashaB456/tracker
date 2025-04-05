f60bb6c74e9746fc7a8d5ecf6bc2188a3e4720b3
SashaB456 <ppppppppppppppdhdh@example.com>
2025-04-05 20:33:04 +0300

    Added editing tasks

--- trackerapp/urls.py
+++ trackerapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('task-list/', views.TaskListView.as_view(),  name='task-list'),
+    path('task/list/', views.TaskListView.as_view(),  name='task-list'),
+    path('task/detail/<int:pk>', views.TaskDetailView.as_view(),  name='task-detail'),
+    path('task/create', views.TaskCreateView.as_view(), name='create-view'),
 ]
--- trackerapp/views.py
+++ trackerapp/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
+from .mixins import UserIsOwnerMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .forms import TaskForm
 # Create your views here.
 class TaskListView(ListView):
     model = Task
     template_name = 'taskapp/task_list.html'
     context_object_name = 'tasks'
-def create_task(request):
-    if request.method == 'POST':
-        name = request.POST.get("title", default='Title')
+    def get_query(self):
+        queryset = Task.objects.filter(status__ne='Done').exclude(a=True)
+        return queryset
+class TaskDetailView(DetailView):
+    model = Task
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'taskapp/task_create.html'
+    success_url = reverse_lazy('task-list')
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.user = user
+        return super().form_valid(form)
+class TaskEditView(UserIsOwnerMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'taskapp/task_create.html'
+    success_url = reverse_lazy('task-list')
